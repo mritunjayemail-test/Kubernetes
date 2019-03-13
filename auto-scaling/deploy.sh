dd30479c39d21e0e405c1677543303895cc431fb
Mritunjay Kumar <kumar@Mritunjays-MBP.lan>
2019-03-13 12:03:09 +0100

    Done

--- auto-scaling/deploy.sh
+++ auto-scaling/deploy.sh
@@ -5,6 +5,11 @@ kubectl get hpa --namespace acg-ns
 
 kubectl get nodes
 
-kubectl run -i --tty loader --image=busybox /bin/sh
 
+# make stress load 
+kubectl run -i --tty loader --image=busybox /bin/sh
 while true; do wget -q -O- http://acg-lb.acg-ns.svc.cluster.local; done 
+
+# CIDR range for node IP
+kubectl get nodes -o jsonpath='{.items[*].spec.externalID}'
+
