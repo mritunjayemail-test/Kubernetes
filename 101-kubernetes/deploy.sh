307ed369528bb3ab7b05cdbba50043bda4497eb7
Mritunjay Kumar <kumar@mritunjay.local>
2019-03-23 23:40:35 +0100

    done

---
+++ 101-kubernetes/deploy.sh
@@ -0,0 +1,27 @@
+
+# Build The Docker Image
+docker  build -t ashupost/node-app:v2 ./docker
+
+# Push Image to docker hub.
+docker push ashupost/node-app:v2
+
+# Run the Docker Container in detached mode.
+docker run -it -d -p 9001:9000 --name web ashupost/node-app:v1
+
+#execute an interactive bash shell on the container.
+docker exec -it web bash
+curl localhost:9000
+exit
+
+# Access The Application while running the docker container
+curl localhost:9001
+
+cd ..
+cd kubernetes
+kubectl apply  -f kubernetes/web-pod.yaml -f kubernetes/web-service.yaml 
+curl $(minikube ip):32001
+kubectl delete -f kubernetes/web-pod.yaml -f kubernetes/web-service.yaml 
+
+kubectl apply  -f kubernetes/web-deployment.yaml -f kubernetes/web-service.yaml 
+curl $(minikube ip):32001
+kubectl delete -f kubernetes/web-deployment.yaml -f kubernetes/web-service.yaml 
