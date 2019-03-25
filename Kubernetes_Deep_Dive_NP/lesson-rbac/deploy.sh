1e29be8891150f1f539714687994663965a2016e
Mritunjay Kumar <kumar@mritunjay.lan>
2019-03-25 06:03:21 +0100

    done

--- Kubernetes_Deep_Dive_NP/lesson-rbac/deploy.sh
+++ Kubernetes_Deep_Dive_NP/lesson-rbac/deploy.sh
@@ -1,7 +1,13 @@
 
+openssl genrsa -out mia.key 2048
+
+openssl req -new -key mia.key -out mia.csr -subj "/CN=mia/O=acg"
+
+openssl x509 -req -in mia.csr -CA /Users/kumar/kubernetes/Kubernetes/Kubernetes_Deep_Dive_NP/lesson-rbac/mia.key -CAcreateserial -out mia.crt -days 365
+
 kubectl config set-credentials mia \
---client-certificate=~/kubernetes/Kubernetes/Kubernetes_Deep_Dive_NP/lesson-rbac/mia.crt \
---client-key=~/kubernetes/Kubernetes/Kubernetes_Deep_Dive_NP/lesson-rbac/mia.key
+--client-certificate=/Users/kumar/kubernetes/Kubernetes/Kubernetes_Deep_Dive_NP/lesson-rbac/mia.csr \
+--client-key=/Users/kumar/kubernetes/Kubernetes/Kubernetes_Deep_Dive_NP/lesson-rbac/mia.key
 
 kubectl config set-context mia \
 --cluster=acg.k8s.local --namespace=acg --user=mia
